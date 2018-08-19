afcfca670525c1ae634923940a2e547a83e3a4d3
Bharath Vullanki <bharath.vullanki@gmail.com>
2018-08-19 11:17:59 -0500

    listening port

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ const PORT = 3000;
 app.get('/',(req,res)=>
 res.send(`Node and Express server is running on port ${PORT}`)
 );
+
+app.listen(PORT,()=>
+    console.log(`your server is running on ${PORT}`)
+);
