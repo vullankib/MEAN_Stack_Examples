fbe5398507f24e1214c7183713b187a413b596be
Bharath Vullanki <bharath.vullanki@gmail.com>
2019-08-30 00:48:12 -0500

    updated index.js

--- index.js
+++ index.js
@@ -2,18 +2,23 @@ import express from 'express';
 import routes from './src/routes/egRoutes.js';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+
 // Send Objects or Data through HTTP Post to the database
 
 
 const app = express();
 const PORT = 3000;
+const url = MONGOLAB_URI;
 
-// Mongoose Connection
-if ( mongoose.connect("mongodb://localhost:27017/egDB") === false )
-{
-useNewUrlParser: true 
-mongoose.connect(dbConn, { promiseLibrary: global.Promise }) === true
-}
+// // Mongoose Connection
+// if ( mongoose.connect(url) === false )
+// {
+// useNewUrlParser: true
+// mongoose.connect(dbConn, { promiseLibrary: global.Promise }) === true
+// }
+
+module.exports.connect = async dsn => mongoose.connection(dsn, {useNewUrlParser:true});
+
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({extended:true}));
